cf472145948ae22bd7e1fa32be48df82857c7494
Nguyễn Minh Trang <pagenguyen219@vnu.edu.vn>
2015-02-06 22:48:16 +0700

    Sign in

---
+++ EasiLendar/www/js/signIn.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+(function() {
+	var app = angular.module('signIn', []);
+	
+	app.controller("SignInController", function($scope) {
+		
+		$scope.isRemember = false;
+		$scope.user = { id: "", password:""};
+		$scope.isShow = false;
+		
+		$scope.validate = function() {
+			return false;
+		};
+		
+		$scope.signIn = function() {
+			if ($scope.validate()) {
+				window.alert("Validate");
+				
+				/* Something here*/
+				
+				window.location.href="../html/home.html";
+			} else {
+				$scope.isShow = true;
+				$scope.user.id = "";
+				$scope.user.password ="";
+			}
+		};
+		
+		$scope.confirm = function() {
+			$scope.isShow = false;
+		}
+		
+	});
+	
+})();
