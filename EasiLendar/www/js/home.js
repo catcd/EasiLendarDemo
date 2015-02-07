94d18f42b84bf80d46ac402d6f51f9ba8993f40e
unknown <catcd_58@vnu.edu.vn>
2015-02-07 22:11:40 +0700

    update coding style

--- EasiLendar/www/js/home.js
+++ EasiLendar/www/js/home.js
@@ -1,82 +1,78 @@
-angular.module('mainAPP', ['ionic'])
+angular.module('mainAPP', [ 'ionic' ])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "app.html"
-    })
-    .state('app.home', {
-      url: "/home",
-      views: {
-        'appContent' :{
-          templateUrl: "home.html",
-          controller : "HomeController"
-        }
-      }
-    })
-    .state('app.setting', {
-      url: "/setting",
-      views: {
-        'appContent' :{
-          templateUrl: "setting.html",
-          controller : "SettingController"
-        }
-      }
-    })
-    .state('app.profile', {
-      url: "/profile",
-      views: {
-        'appContent' :{
-          templateUrl: "profile.html",
-          controller : "ProfileController"
-        }
-      }
-    })
-  
-  $urlRouterProvider.otherwise("/app/home");
+	$stateProvider.state('app', {
+		url : "/app",
+		abstract : true,
+		templateUrl : "app.html"
+	}).state('app.home', {
+		url : "/home",
+		views : {
+			'appContent' : {
+				templateUrl : "home.html",
+				controller : "HomeController"
+			}
+		}
+	}).state('app.setting', {
+		url : "/setting",
+		views : {
+			'appContent' : {
+				templateUrl : "setting.html",
+				controller : "SettingController"
+			}
+		}
+	}).state('app.profile', {
+		url : "/profile",
+		views : {
+			'appContent' : {
+				templateUrl : "profile.html",
+				controller : "ProfileController"
+			}
+		}
+	})
+
+	$urlRouterProvider.otherwise("/app/home");
 })
 
 .controller('sideMenuController', function($scope, $ionicSideMenuDelegate) {
-  $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+	$scope.toggleLeft = function() {
+		$ionicSideMenuDelegate.toggleLeft();
+	};
 })
 
 .controller("HomeController", function($scope) {
-  
+
 })
 
 .controller("SettingController", function($scope) {
-  
+
 })
 
 .controller("ProfileController", function($scope) {
-  
+
 })
 
 .controller('PopOverController', function($scope, $ionicPopover) {
-  $scope.popover = $ionicPopover.fromTemplate(template, {
-    scope: $scope,
-  });
+	$scope.popover = $ionicPopover.fromTemplate(template, {
+		scope : $scope,
+	});
 
-  // .fromTemplateUrl() method
-  $ionicPopover.fromTemplateUrl('my-popover.html', {
-    scope: $scope,
-  }).then(function(popover) {
-    $scope.popover = popover;
-  });
+	// .fromTemplateUrl() method
+	$ionicPopover.fromTemplateUrl('my-popover.html', {
+		scope : $scope,
+	}).then(function(popover) {
+		$scope.popover = popover;
+	});
 
-  $scope.openPopover = function($event) {
-    $scope.popover.show($event);
-  };
-  $scope.closePopover = function() {
-    $scope.popover.hide();
-  };
-  //Cleanup the popover when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.popover.remove();
-  });
+	$scope.openPopover = function($event) {
+		$scope.popover.show($event);
+	};
+	$scope.closePopover = function() {
+		$scope.popover.hide();
+	};
+	//Cleanup the popover when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.popover.remove();
+	});
 });
