e0f28d893e0881d2a15ad134a2dd87a27843983b
unknown <catcd_58@vnu.edu.vn>
2015-02-24 23:28:12 +0700

    delete all junk file

--- EasiLendar/www/js/controllers/SearchController.js
+++
@@ -1,8 +0,0 @@
-angular.module('MainApp.controllers.search', [])
-
-.controller("SearchController", function($scope, $ionicPopup){
-	$scope.fShow = false;
-	$scope.showAdvanceFilter = function(){
-		$scope.fShow = !($scope.fShow);
-	}
-})
--- EasiLendar/www/js/searchFilterApp.js
+++
@@ -1,9 +0,0 @@
-var App = angular.module("filterApp",["ionic"]);
-App.controller("filterController",["$scope",filterController]);
-
-function filterController($scope){
-	$scope.fShow = false;
-	$scope.showAdvanceFilter = function(){
-		$scope.fShow = !($scope.fShow);
-	}
-}
