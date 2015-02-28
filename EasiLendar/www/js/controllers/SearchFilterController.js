1b580e8cbb412f70ea55fe0309d68b62de0fd841
dungnd_58 <dungnd_58@bitbucket.org>
2015-02-28 12:17:01 +0700

    update search-filter

--- EasiLendar/www/js/controllers/SearchFilterController.js
+++ EasiLendar/www/js/controllers/SearchFilterController.js
@@ -5,11 +5,101 @@
  * type: paticular controller
  */
 
-angular.module('MainApp.controllers.searchFilter', [])
+angular.module('MainApp.controllers.searchFilter', ['ngAnimate'])
 
-.controller("SearchFilterController", function($scope, $ionicPopup) {
-    $scope.fShow = false;
-    $scope.showAdvanceFilter = function() {
-        $scope.fShow = !($scope.fShow);
-    }
+.controller("SearchFilterController", function($rootScope, $scope, $ionicPopup) {
+   		$rootScope.allValues = {
+			mTitle :'',
+			mDuration : '',		//hours
+			mLocation : '',
+			mMessage : '',
+			mFrom : '',			//hours
+			mTo : '',			//hours
+			mExpiration : '',	//hours
+			mDate : '',  		//Days/Weeks/Months/Years
+			mBreakfast : 'none',
+			mLunch : 'none',		//Avoid/Prioritize
+			mDinner : 'none',
+			mOffice : 'none',
+			mHoliday : 'none'
+		};
+
+		$scope.NewValues = {
+			mTitle :'',
+			mDuration : '',		//hours
+			mLocation : '',
+			mMessage : '',
+			mFrom : '',			//hours
+			mTo : '',			//hours
+			mExpiration : '',	//hours
+			mDate : '',  		//Days/Weeks/Months/Years
+			mBreakfast : 'none',
+			mLunch : 'none',		//Avoid/Prioritize
+			mDinner : 'none',
+			mOffice : 'none',
+			mHoliday : 'none'
+		};
+
+		$scope.unVip = false;
+		$scope.mShow = false;
+		
+		/*$scope.passValue = function(){
+			alert($rootScope.allValues.mTitle + $rootScope.allValues.mDuration + $rootScope.allValues.mLocation + $rootScope.allValues.mMessage);
+		}*/
+		$scope.deleteValue = function(){
+			$rootScope.allValues = angular.copy($scope.NewValues);
+			//alert($rootScope.allValues.mTitle + $rootScope.allValues.mDuration + $rootScope.allValues.mLocation + $rootScope.allValues.mMessage);
+		}
+
+		$('label').mousedown(function() { 
+ 			var x = '#' + $(this).attr("for");
+ 			var $input = $(x); 
+ 			if($input.prop("checked") == true){
+ 				$(this).mouseup(function(){
+ 					setTimeout(function(){
+ 						$input.prop('checked',false);
+ 					},5);
+ 				});
+	 			var modelName = $input.attr("data-ng-model");
+			 	if(modelName == 'allValues.mBreakfast'){
+			 		$scope.allValues.mBreakfast = 'none';
+			 	}
+			 	else if(modelName == 'allValues.mLunch'){
+			 		$scope.allValues.mLunch = 'none';
+			 	}
+			 	else if(modelName == 'allValues.mDinner'){
+			 		$scope.allValues.mDinner = 'none';
+			 	}
+			 	else if(modelName == 'allValues.mOffice'){
+			 		$scope.allValues.mOffice = 'none';
+			 	}
+			 	else if(modelName == 'allValues.mHoliday'){
+			 		$scope.allValues.mHoliday = 'none';
+			 	}
+			 	else {} 
+ 			}
+ 			else{
+ 				$(this).mouseup(function(){
+ 					setTimeout(function(){
+ 						$input.prop('checked',true);
+ 					},5);
+ 				});
+ 			}
+ 		});
 })
+.directive('slideToggle',function(){
+		return{
+			restrict: 'A',
+			scope: {
+				isOpen: '=slideToggle'
+			},
+			link: function(scope, element, attr) {
+		      element.hide();  
+		      scope.$watch('isOpen', function(newVal,oldVal){
+		        if(newVal !== oldVal){ 
+		          element.stop().slideToggle("slow");
+		        }
+		      });
+		    }
+		};
+});
