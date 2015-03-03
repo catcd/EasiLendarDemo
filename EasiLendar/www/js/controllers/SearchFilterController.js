3ef5c587c247a0a21a89a49dba435b7cc48b5dbb
dungnd_58 <dungnd_58@bitbucket.org>
2015-03-04 00:00:02 +0700

    update searching filter

--- EasiLendar/www/js/controllers/SearchFilterController.js
+++ EasiLendar/www/js/controllers/SearchFilterController.js
@@ -1,7 +1,7 @@
 /**
  * starter: Can Duy Cat
  * owner: Ngo Duc Dung
- * last update: 28/02/2015
+ * last update: 04/03/2015
  * type: paticular controller
  */
 
@@ -10,69 +10,86 @@ angular.module('MainApp.controllers.searchFilter', ['ngAnimate'])
 .controller("SearchFilterController", function($rootScope, $scope, $ionicPopup) {
    		$rootScope.allValues = {
 			mTitle :'',
-			mDuration : '',		//hours
+			mDuration: '',
+			mDurationMinute : '',	//minute	
+			mDurationHour: '',     	//hour
 			mLocation : '',
 			mMessage : '',
-			mFrom : '',			//hours
-			mTo : '',			//hours
-			mExpiration : '',	//hours
-			mDate : '',  		//Days/Weeks/Months/Years
-			mBreakfast : 'none',
-			mLunch : 'none',		//Avoid/Prioritize
-			mDinner : 'none',
-			mOffice : 'none',
-			mHoliday : 'none'
+			mFrom : '',			
+			mFromHour : '',			//hour
+			mFromMinute : '',		//minute
+			mTo : '',		
+			mToHour : '',			//hour
+			mToMinute : '',			//minute
+			mExpiration : '',		
+			mExpirationDay : '',	//day
+			mExpirationWeek : '',	//week
+			mExpirationMonth : '',	//month
+			mExpirationYear : '',	//year
+			mBreakfast : null,
+			mLunch : null,			//Avoid=true /Prioritize=false
+			mDinner : null,
+			mOffice : null,
+			mHoliday : null
 		};
 
 		$scope.newValues = angular.copy($rootScope.allValues);
 		$scope.unVip = false;
 		$scope.mShow = false;
+		$scope.min = 0;
+
+		$scope.$watch('allValues.mDuration',function(){
+			$rootScope.allValues.mDurationHour = parseInt($rootScope.allValues.mDuration / 60);
+			$rootScope.allValues.mDurationMinute = $rootScope.allValues.mDuration % 60;
+		});
+
+		$scope.$watch('allValues.mTo',function(){
+			if($rootScope.allValues.mTo > $rootScope.allValues.mFrom){
+				$rootScope.allValues.mToHour = parseInt($rootScope.allValues.mTo / 60);
+				$rootScope.allValues.mToMinute = $rootScope.allValues.mTo % 60;
+			} 
+		})
+
+		$scope.$watch('allValues.mFrom',function(){
+			$scope.min = $rootScope.allValues.mFrom;
+
+			if($rootScope.allValues.mFrom < $rootScope.allValues.mFrom){
+				$rootScope.allValues.mFromHour = parseInt($rootScope.allValues.mFrom / 60);
+				$rootScope.allValues.mFromMinute = $rootScope.allValues.mFrom % 60;
+			} 
+		})
+
+		$scope.$watch('allValues.mExpiration',function(){
+			var date = $rootScope.allValues.mExpiration;
+			$rootScope.allValues.mExpirationYear = parseInt(date / 365);
+			date = date % 365;
+			$rootScope.allValues.mExpirationMonth = parseInt(date / 31);
+			date = date % 31;
+			$rootScope.allValues.mExpirationWeek = parseInt(date / 7);
+			$rootScope.allValues.mExpirationDay = date % 7;
+		});
 		
-		/*$scope.passValue = function(){
-			alert($rootScope.allValues.mTitle + $rootScope.allValues.mDuration + $rootScope.allValues.mLocation + $rootScope.allValues.mMessage);
-		}*/
-		$scope.deleteValue = function(){
+		$scope.passValue = function(){
+		/*alert($rootScope.allValues.mDurationHour 
+				+ ':' + $rootScope.allValues.mDurationMinute + ', ' +  $rootScope.allValues.mFromHour 
+				+ ':' + $rootScope.allValues.mFromMinute + ', ' + $rootScope.allValues.mToHour + ':' + $rootScope.allValues.mToMinute 
+				+ ', ' +  $rootScope.allValues.mExpirationDay + ', ' + $rootScope.allValues.mExpirationWeek 
+				+ ', ' + $rootScope.allValues.mExpirationMonth + ', ' + $rootScope.allValues.mExpirationYear);
+		*/
+		}
+		
+		$scope.deleteValue = function(form){
+			/*RESET VALUE*/
 			$rootScope.allValues = angular.copy($scope.newValues);
 			//alert($rootScope.allValues.mTitle + $rootScope.allValues.mDuration + $rootScope.allValues.mLocation + $rootScope.allValues.mMessage);
-			$scope.newValues = null;
-		}
+			delete $scope.newValues;
 
-		$('label').mousedown(function() { 
- 			var x = '#' + $(this).attr("for");
- 			var $input = $(x); 
- 			if($input.prop("checked") == true){
- 				$(this).mouseup(function(){
- 					setTimeout(function(){
- 						$input.prop('checked',false);
- 					},5);
- 				});
-	 			var modelName = $input.attr("data-ng-model");
-			 	if(modelName == 'allValues.mBreakfast'){
-			 		$scope.allValues.mBreakfast = 'none';
-			 	}
-			 	else if(modelName == 'allValues.mLunch'){
-			 		$scope.allValues.mLunch = 'none';
-			 	}
-			 	else if(modelName == 'allValues.mDinner'){
-			 		$scope.allValues.mDinner = 'none';
-			 	}
-			 	else if(modelName == 'allValues.mOffice'){
-			 		$scope.allValues.mOffice = 'none';
-			 	}
-			 	else if(modelName == 'allValues.mHoliday'){
-			 		$scope.allValues.mHoliday = 'none';
-			 	}
-			 	else {} 
- 			}
- 			else{
- 				$(this).mouseup(function(){
- 					setTimeout(function(){
- 						$input.prop('checked',true);
- 					},5);
- 				});
- 			}
- 		});
+			/* RESET FORM*/
+			form.$setPristine();
+      		form.$setUntouched();
+		}
 })
+
 .directive('slideToggle',function(){
 		return{
 			restrict: 'A',
@@ -88,4 +105,47 @@ angular.module('MainApp.controllers.searchFilter', ['ngAnimate'])
 		      });
 		    }
 		};
+})
+
+.directive('checkUncheckRadio', function(){
+	return{
+		restrict: 'A',
+		link: function(scope,element,attr){
+			var labelRadio = element.next();
+
+			labelRadio.bind('mousedown',function(){
+				
+				//alert(element.prop("checked"));
+				
+				if(element.prop("checked") == true){
+	 				labelRadio.bind('mouseup',function(){
+	 					setTimeout(function(){
+	 						element.prop('checked',false);
+	 					},5);
+	 				});
+
+	 				
+	 				var modelName = element.attr("data-ng-model");
+	 				/*
+	 				if(modelName == 'color1'){
+	 					rootScope.color1 = 'none';
+	 				}
+	 				else if(modelName == 'color2'){
+	 					rootScope.color2 = 'none';
+	 				}
+	 				
+	 				else{}
+	 				*/
+	 			}
+
+	 			else{
+	 				labelRadio.bind('mouseup',function(){
+	 					setTimeout(function(){
+	 						element.prop('checked',true);
+	 					},5);
+	 				});
+	 			}	   
+			});
+		}
+	};
 });
