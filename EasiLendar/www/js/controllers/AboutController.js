fbe864fb764869ca737c87c6a5505e44e93a9a1a
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-03-12 16:56:07 +0700

    1.1.15[139] Additional file to the last commit (64b73a7)
    - register.html
    - change version in about

--- EasiLendar/www/js/controllers/AboutController.js
+++ EasiLendar/www/js/controllers/AboutController.js
@@ -1,12 +1,12 @@
 /**
  * starter: Can Duy Cat
  * owner: Can Duy Cat
- * last update: 12/03/2015
+ * last update: 11/03/2015
  * type: about controller (version)
  */
 
 angular.module('MainApp.controllers.about', [])
 
 .controller("AboutController", function($scope) {
-    $scope.ver = "1.1.15.[138]"
+    $scope.ver = "1.1.15.[139]"
 })
