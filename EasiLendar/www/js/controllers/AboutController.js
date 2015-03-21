48358ab32bf365cdcc87dc2385384a9c4eeacfa4
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-03-21 20:02:36 +0700

    1.2.27.[171] Update DataBase
    Add $rootScope.update() function

--- EasiLendar/www/js/controllers/AboutController.js
+++ EasiLendar/www/js/controllers/AboutController.js
@@ -9,5 +9,5 @@
 angular.module('MainApp.controllers.about', [])
 
 .controller("AboutController", function($scope) {
-	$scope.ver = "1.2.27.[169]"
+	$scope.ver = "1.2.27.[171]";
 })
--- EasiLendar/www/js/shareds/DataBase.js
+++ EasiLendar/www/js/shareds/DataBase.js
@@ -5,8 +5,30 @@
  * type: all shared database variables and functions
  */
 
-angular.module('MainApp.shareds.dataBase', [])
+var database = angular.module('MainApp.shareds.dataBase', []);
 
-.run(function($rootScope/*, $ionicPopup, $timeout, $state, $ionicPlatform, $ionicHistory*/) {
-
-})
+database.run (function($rootScope) {
+	/*
+	 * Update function
+	 * Only call when isLogin = true
+	 * save every data of user in server
+	 */
+	$rootScope.update = function() {
+		if ($rootScope.eUser.uID != "" 
+				&& typeof($rootScope.eUser.uID) != "undefined") {
+			
+			var ref = new Firebase(
+					"https://radiant-inferno-3243.firebaseio.com/Users/"
+					+ $rootScope.eUser.uID);
+			ref.set({
+				name : $rootScope.eUser.uName,
+				password : $rootScope.eUser.uPassword,
+				local_calendar : $rootScope.eUser.uLocalCalendar,
+				g_calendar : $rootScope.eUser.uGmailCalendar,
+				friends : $rootScope.eUser.uFriend,
+				VIP : $rootScope.eUser.uVIP,
+				gmail : $rootScope.eUser.uEmail,
+			});
+		}
+	}
+});
