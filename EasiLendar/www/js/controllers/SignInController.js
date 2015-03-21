1b07daf42e6b1470a49a60ccde537aafa20992b1
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-03-21 21:24:27 +0700

    1.2.27.[172] Update Sign In
    - Add show loading function
    - Welcome message will disappear after 3 secs

--- EasiLendar/www/js/controllers/SignInController.js
+++ EasiLendar/www/js/controllers/SignInController.js
@@ -1,14 +1,14 @@
 /**
  * starter: Can Duy Cat 
  * owner: Nguyen Minh Trang 
- * last update: 17/03/2015 
+ * last update: 21/03/2015 
  * type: particular controller
  */
 
 var signIn = angular.module('MainApp.controllers.signIn', [ 'ionic' ]);
 
 signIn.controller('SignInController',
-		function($rootScope, $scope, $timeout, $http, $state, $ionicPopup) 
+		function($rootScope, $scope, $timeout, $http, $state, $ionicLoading, $ionicPopup) 
 {
 	// link to home's default view
 	var link = $rootScope.eSettings.sDefaultView;
@@ -34,6 +34,150 @@ signIn.controller('SignInController',
 		}
 	};
 
+	// create new user
+	$scope.user = new User();
+	
+	// to form
+	$scope.toForm = function() {
+		$state.go('form');
+	};
+	
+	// to warning
+	$scope.toWarning = function() {
+		$state.go('warning');
+	};
+	
+	// show loading balls
+	$scope.loading = function() {
+		$ionicLoading.show({
+			template: '<div id="followingBallsG"><div id="followingBallsG_1" class="followingBallsG"></div><div id="followingBallsG_2" class="followingBallsG"></div><div id="followingBallsG_3" class="followingBallsG"></div><div id="followingBallsG_4" class="followingBallsG"></div></div>',
+			hideOnStateChange: true,
+		});
+	};
+	
+	// show alert
+	$scope.showAlert = function() {
+		var alertPopup = $ionicPopup.alert({
+		     title: 'Registered!',
+		     template: 'Welcome to EasiLendar!',
+		});
+		$timeout(function() {
+		     alertPopup.close(); //close the popup after 3 seconds for some reason
+		}, 3000);
+	};
+	
+	// sign in function with firebase
+	$scope.signIn = function() {
+		$scope.loading();
+		if (!$scope.user.checkChar()) {
+			$scope.toWarning();
+		} else {
+			var id = $scope.user.id;
+			var pass = $scope.user.password;
+			var ref = new Firebase(
+					"https://radiant-inferno-3243.firebaseio.com/Users/" + id);
+
+			ref.on("value", function(snapshot) {
+				var user = snapshot.val();
+				if (user == null || user.password != pass) {
+					$scope.toWarning();
+				} else {
+					// copy all user's data to $rootScope
+					$rootScope.eUser.uID = id;
+					$rootScope.eUser.uPassword = pass;
+					$rootScope.eUser.uRemember = $scope.isRemember;
+					$rootScope.eUser.uEmail = user.gmail;
+					$rootScope.eUser.uName = user.name;
+					$rootScope.eUser.uFriend = user.friends;
+					$rootScope.eUser.uGmailCalendar = user.g_calendar;
+					$rootScope.eUser.uLocalCalendar = user.local_calendar;
+					$rootScope.eUser.uVIP = user.VIP;
+					
+					$rootScope.eUser.isLogin = true;
+					
+					$scope.user.reset();
+					$state.go(link);
+				}
+			}, function(errorObject) {
+				$scope.toWarning();
+			});
+		}
+	};
+	
+	// register function
+	$scope.register = function() {
+		$scope.loading();
+		// reset all warnings
+		for (var i = 0; i < 5; i++) {
+			$scope.warnings.reset(i);
+		}
+
+		// flag array, contains temporary warning messages
+		var flag = [];
+		// check if there is a warning
+		var check = false;
+
+		flag[0] = $scope.user.checkID(); // ID: true or message
+		flag[1] = $scope.user.checkName(); // Name: true or message
+		flag[2] = $scope.user.checkEmail(); // Email: true or message
+		flag[3] = $scope.user.checkPass(); // Password: true or message
+		flag[4] = $scope.user.checkCPass(); // Confirm password: true or message
+
+		// if flag[i] is a string => it's a warning
+		for (var i = 0; i < 5; i++) {
+			if (typeof (flag[i]) == "string") {
+				$scope.warnings.mes[i] = flag[i];
+				check = true;
+			}
+		}
+
+		// check inputs are correct (or else all 
+		//users will be licked)
+		if (!check) {
+			// create a reference to Firebase
+			var ref = new Firebase(
+					"https://radiant-inferno-3243.firebaseio.com/Users/"
+							+ $scope.user.id);
+
+			// get data from that link if exists, null if not
+			ref.on('value', function(snapshot) {
+				// if id existed => change ID message
+				if (snapshot.val() != null) {
+					$scope.warnings.mes[0] = "Existed";
+					check = true;
+				}
+				// if there is no warning
+				if (!check) {
+					// cut email string to save (get rid of
+					// '@gmail.com')
+					var pos = $scope.user.email.search("@");
+					var mail;
+					if (pos == -1) {
+						mail = $scope.user.email;
+					} else {
+						mail = $scope.user.email.slice(0, pos);
+					}
+
+					// create new user
+					ref.set({
+						name : $scope.user.name,
+						password : $scope.user.password,
+						local_calendar : "",
+						g_calendar : "",
+						friends : "",
+						VIP : 0,
+						gmail : mail,
+					});
+					
+					// welcome message
+					$scope.showAlert();
+					$scope.user.reset();
+					$scope.toForm();
+				}
+			});
+		}
+	};
+	
 	/* class User */
 	function User() {
 		// all informations
@@ -154,48 +298,7 @@ signIn.controller('SignInController',
 			return true;
 		};
 	}; // End of class User
-
-	// create new user
-	$scope.user = new User();
 	
-	
-	// some thing new
-	
-	// sign in function with firebase
-	$scope.signIn = function() {
-		if (!$scope.user.checkChar()) {
-			$state.go('warning');
-		} else {
-			var id = $scope.user.id;
-			var pass = $scope.user.password;
-			var ref = new Firebase(
-					"https://radiant-inferno-3243.firebaseio.com/Users/" + id);
-
-			ref.on("value", function(snapshot) {
-				var user = snapshot.val();
-				if (user == null || user.password != pass) {
-					$state.go("warning");
-				} else {
-					$rootScope.eUser.uID = id;
-					$rootScope.eUser.uPassword = pass;
-					$rootScope.eUser.uRemember = $scope.isRemember;
-					$rootScope.eUser.uEmail = user.gmail;
-					$rootScope.eUser.uName = user.name;
-					$rootScope.eUser.uFriend = user.friends;
-					$rootScope.eUser.uGmailCalendar = user.g_calendar;
-					$rootScope.eUser.uLocalCalendar = user.local_calendar;
-					$rootScope.eUser.uVIP = user.VIP;
-					
-					$rootScope.eUser.isLogin = true;
-					
-					$scope.user.reset();
-					$state.go(link);
-				}
-			}, function(errorObject) {
-				$state.go("warning");
-			});
-		}
-	};
 	/*
 	 * // sign in function $scope.signIn = function() { if
 	 * (!$scope.user.checkChar()) { $state.go('warning'); } else { var id =
@@ -205,92 +308,4 @@ signIn.controller('SignInController',
 	 * $state.go(link); } else { $state.go(link); // should be warning } })
 	 * .error(function(data,status) { $state.go(link); // should be warning }); } };
 	 */
-
-	// confirm the warning when sign in
-	$scope.confirm = function() {
-		$state.go('form');
-	};
-
-	// register function
-	$scope.register = function() {
-		// reset all warnings
-		for (var i = 0; i < 5; i++) {
-			$scope.warnings.reset(i);
-		}
-
-		// flag array, contains temporary warning messages
-		var flag = [];
-		// check if there is a warning
-		var check = false;
-
-		// ID: true or message
-		flag[0] = $scope.user.checkID();
-		// Name: true or message
-		flag[1] = $scope.user.checkName();
-		// Email: true or message
-		flag[2] = $scope.user.checkEmail();
-		// Password: true or message
-		flag[3] = $scope.user.checkPass();
-		// Confirm password: true or message
-		flag[4] = $scope.user.checkCPass();
-
-		// if flag[i] is a string => it's a warning
-		for (var i = 0; i < 5; i++) {
-			if (typeof (flag[i]) == "string") {
-				$scope.warnings.mes[i] = flag[i];
-				check = true;
-			}
-		}
-
-		// if inputs are correct (or else all users will be
-		// licked)
-		if (!check) {
-			// create a reference to Firebase
-			var ref = new Firebase(
-					"https://radiant-inferno-3243.firebaseio.com/Users/"
-							+ $scope.user.id);
-
-			// get data from that link if exists, null if not
-			ref.on('value', function(snapshot) {
-				// if id existed => change ID message
-				if (snapshot.val() != null) {
-					$scope.warnings.mes[0] = "Existed";
-					check = true;
-				}
-				// if there is no warning
-				if (!check) {
-					// cut email string to save (get rid of
-					// '@gmail.com')
-					var pos = $scope.user.email.search("@");
-					var mail;
-					if (pos == -1) {
-						mail = $scope.user.email;
-					} else {
-						mail = $scope.user.email.slice(0, pos);
-					}
-
-					// create new user
-					ref.set({
-						name : $scope.user.name,
-						password : $scope.user.password,
-						local_calendar : "",
-						g_calendar : "",
-						friends : "",
-						VIP : 0,
-						gmail : mail
-					});
-					
-					// welcome message
-					$scope.showAlert("Welcome to EasiLendar!");
-					$scope.user.reset();
-					$state.go('form');
-				}
-			});
-		}
-	};
-
-	// back to form
-	$scope.back = function() {
-		$state.go('form');
-	};
 });
