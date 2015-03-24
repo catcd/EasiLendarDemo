127b04526eb4156a49c9b10fbc404daf1d305790
unknown <catcd_58@vnu.edu.vn>
2015-03-25 00:59:24 +0700

    1.2.31.[182] Update noti full design
    - Correct design for each tab
    
    - Change tab work
    
    - Action sheet work

--- EasiLendar/www/js/controllers/AboutController.js
+++ EasiLendar/www/js/controllers/AboutController.js
@@ -2,12 +2,12 @@
  * starter: Can Duy Cat
  * owner: Can Duy Cat
  * everyone can update in assigned time
- * last update: 24/03/2015
+ * last update: 25/03/2015
  * type: about controller (version)
  */
 
 angular.module('MainApp.controllers.about', [])
 
 .controller("AboutController", function($scope) {
-	$scope.ver = "1.2.30.[181]";
+	$scope.ver = "1.2.31.[182]";
 })
--- EasiLendar/www/js/controllers/PopOverController.js
+++ EasiLendar/www/js/controllers/PopOverController.js
@@ -1,13 +1,13 @@
 /**
  * starter: Can Duy Cat
  * owner: Can Duy Cat
- * last update: 24/03/2015
+ * last update: 25/03/2015
  * type: popover controller
  */
 
 angular.module('MainApp.controllers.popover', [])
 
-.controller('PopOverController', function($scope, $ionicPopover) {
+.controller('PopOverController', function($rootScope, $scope, $ionicPopover, $ionicActionSheet, $timeout) {
     /**
      * popover variable
      */
@@ -16,7 +16,7 @@ angular.module('MainApp.controllers.popover', [])
     $scope.mPopoverStatus[true] = "active";
     $scope.mPopoverStatus[false] = "";
 
-    $scope.mPopoverActive = "friend";
+    $scope.mPopoverActive = "";
 
     /**
      * popover function
@@ -40,6 +40,7 @@ angular.module('MainApp.controllers.popover', [])
     });
 
     $scope.openPopover = function($event) {
+        $scope.tabActive("friend");
         $scope.friendPopover.show($event);
     };
     $scope.closePopover = function() {
@@ -49,4 +50,98 @@ angular.module('MainApp.controllers.popover', [])
     $scope.$on('$destroy', function() {
         $scope.friendPopover.remove();
     });
+
+    /**
+     * action sheet
+     */
+    // friend action sheet
+    $scope.friendAction = function(name) {
+        // Show the action sheet
+        var friendSheet = $ionicActionSheet.show({
+            buttons: [{
+                text: 'View'
+            }, {
+                text: 'Comfirm request'
+            }],
+            destructiveText: 'Delete request',
+            titleText: name + " request",
+            cancelText: 'Cancel',
+            cancel: function() {
+                // TODO cancel code here
+            },
+            destructiveButtonClicked: function() {
+                // TODO delete code here
+                $rootScope.showAlert("Deleted");
+
+                return true;
+            },
+            buttonClicked: function(index) {
+                if (index == 0) {
+                    // TODO view code here
+                    $rootScope.showAlert("Viewing");
+                    $scope.closePopover();
+                } else {
+                    // TODO confirm code here
+                    $rootScope.showAlert("Comfirm friend request");
+                }
+
+                return true;
+            }
+        });
+        // hide the sheet after five seconds
+        $timeout(function() {
+            friendSheet();
+        }, 5000);
+
+    };
+
+    // Request action sheet
+    $scope.requestAction = function(name) {
+        // Show the action sheet
+        var requestSheet = $ionicActionSheet.show({
+            buttons: [{
+                text: 'View detail'
+            }, {
+                text: 'Accept'
+            }],
+            destructiveText: 'Reject',
+            titleText: name + " appointment",
+            cancelText: 'Cancel',
+            cancel: function() {
+                // TODO cancel code here
+            },
+            destructiveButtonClicked: function() {
+                // TODO delete code here
+                $rootScope.showAlert("Deleted");
+
+                return true;
+            },
+            buttonClicked: function(index) {
+                if (index == 0) {
+                    // TODO view code here
+                    $rootScope.showAlert("Viewing");
+                    $scope.closePopover();
+                } else {
+                    // TODO confirm code here
+                    $rootScope.showAlert("Accepted appointment");
+                }
+
+                return true;
+            }
+        });
+        // hide the sheet after two seconds
+        $timeout(function() {
+            requestSheet();
+        }, 5000);
+
+    };
 })
+
+.directive('notiContent', function() {
+    return {
+        restrict: 'E',
+        templateUrl: function(elem, attr) {
+            return "templates/noti-" + attr.type + "-tab.html";
+        }
+    };
+});
