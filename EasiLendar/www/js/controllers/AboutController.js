83ce7ee61f35e784800016b8bea8bec166522180
dungnd58 <dungnd_58@vnu.edu.vn>
2015-03-26 11:08:32 +0700

    1.2.33.[187] fix bug in list calendar and update TimeNode class

--- EasiLendar/www/js/controllers/AboutController.js
+++ EasiLendar/www/js/controllers/AboutController.js
@@ -2,12 +2,12 @@
  * starter: Can Duy Cat
  * owner: Can Duy Cat
  * everyone can update in assigned time
- * last update: 25/03/2015
+ * last update: 26/03/2015
  * type: about controller (version)
  */
 
 angular.module('MainApp.controllers.about', [])
 
 .controller("AboutController", function($scope) {
-	$scope.ver = "1.2.33.[186]";
+	$scope.ver = "1.2.33.[187]";
 })
--- EasiLendar/www/js/shareds/TimeHeap.js
+++ EasiLendar/www/js/shareds/TimeHeap.js
@@ -1,7 +1,7 @@
 /**
  * starter: Can Duy Cat
  * owner: Ngo Duc Dung
- * last update: 25/03/2015
+ * last update: 26/03/2015
  * type: TimeHeap object, Time Node object
  */
 
@@ -14,37 +14,45 @@ angular.module('MainApp.shareds.timeHeap', [])
 				all basic getting, setting function (getEnd(), setEnd(object Date), etc.)
 	 * constructor: TimeNode(start, end); auto rate the time and save to score
 	 */
-	function TimeNode(start, end){
-		//auto rate the time and save to score
-		var rateScore = function(){
-			var scoreArray = [
-							{start: 0, end: 6, pts: 0},
+	function TimeNode(mStart, mEnd){
+		var scoreArray = [	{start: 0, end: 6, pts: 0},
 							{start: 6, end: 8, pts: 5}, {start: 8, end: 11, pts: 10},
 							{start: 11, end: 14, pts: 2}, {start: 14, end: 17, pts: 20},
-							{start: 17, end: 20, pts: 5}, {start: 20, end: 24, pts: 10}
-							];
-			var startTime = start.getHours() * 60 + start.getMinutes();
-			var endTime = end.getHours() * 60 + end.getMinutes();
+							{start: 17, end: 20, pts: 5}, {start: 20, end: 23, pts: 10}  ];
+		//auto rate the time and save to score
+		var rateScore = function(start,end){
 			var sumPts = 0;
 			
-			for(var i=0; i < scoreArray.length; i++){
-				if(start.getHours() > scoreArray[i].end || end.getHours() < scoreArray[i].start) { continue; }
-				else {
-					if(i=0) { sumPts = sumPts; }
+			if( start.getDate() < end.getDate() ) {
+				var endDay = new Date(start.setHours(23,0,0,0));
+				var startDay = new Date(start.setHours(24,0,0,0));  
+				sumPts = rateScore(start,endDay) + rateScore(startDay,end) - 9; 
+			}
+			else if ( start.getDate() > end.getDate() ) { 
+				sumPts = rateScore(end, start); 
+			}
+			else{
+				var startTime = start.getHours() * 60 + start.getMinutes();
+				var endTime = end.getHours() * 60 + end.getMinutes();
+				for(var i=0; i < scoreArray.length; i++){
+					if(start.getHours() > scoreArray[i].end || end.getHours() < scoreArray[i].start) { continue; }
 					else {
-						sumPts = sumPts + (end.getHours() < scoreArray[i].end ? endTime:scoreArray[i].end * 60)
-										- (startTime.getHours() > scoreArray[i].start ? startTime:scoreArray[i].start * 60)
-										* scoreArray[i].pts;
+						if(i==0) { sumPts = sumPts; }
+						else {
+							sumPts = sumPts + (end.getHours() < scoreArray[i].end ? endTime:scoreArray[i].end * 60)
+											- (startTime.getHours() > scoreArray[i].start ? startTime:scoreArray[i].start * 60)
+											* scoreArray[i].pts;
+						}
 					}
 				}
 			}
-			
+
 			return sumPts;
 		};
 
-		this.start = start;
-		this.end = end;
-		this.score = rateScore();
+		this.start = mStart;
+		this.end = mEnd;
+		this.score = rateScore(mStart,mEnd);
 
 		this.getStart = function(){
 			return this.start;
@@ -75,19 +83,22 @@ angular.module('MainApp.shareds.timeHeap', [])
 				function: push (+ upHeap), pop (+ downHeap), getTop, backUp and all basic function
 	 * constructor: TimeHeap(mDate); auto contruct the heap with this.date is Monday of the week has mDate
 	 */
+	//TimeHeap array before using pop 
+	var cache = new Array();
+
 	function TimeHeap(mDate){
 		//find monday of the week has mDate
-		var findMonday = function(){
-			var d = mDate;
+		var findMonday = function(date){
+			var d = date;
 			var monday = d.getDate() - d.getDay() + 1;
 			d = new Date(d.setDate(monday));
 			return d;
 		};
 
-		this.date = findMonday();
+		this.date = findMonday(mDate);
 		this.timeList = new Array();
 		this.length = 0;
-		var cache = new Array();
+		cache[this.date] = new Array();
 
 		//push an item to bottom of array
 		this.push = function(item){
@@ -106,7 +117,7 @@ angular.module('MainApp.shareds.timeHeap', [])
 				j = ( i - (i%2==0 ? 2:1) ) / 2;
 			}
 
-			cache = this.timeList;
+			cache[this.date] = this.timeList;
 		};
 
 		//return and delete an item on top of array
@@ -133,9 +144,11 @@ angular.module('MainApp.shareds.timeHeap', [])
 			return top;
 		};
 
+		//parameter is date in the week that want to back up
 		//return all item in array as before using pop
-		this.backUp = function(){
-			this.timeList = cache;
+		this.backUp = function(date){
+			var index = findMonday(date);
+			this.timeList = cache[index];
 		};
 
 		//return length of array
