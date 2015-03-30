62429547c191c15030cc63e093905ea5534c91ec
unknown <catcd_58@vnu.edu.vn>
2015-03-31 03:05:22 +0700

    1.2.40.[200] Change state change function call
    - Delete all ui-sref
    
    - Use goToState('state') and goHome()

--- EasiLendar/www/js/controllers/AboutController.js
+++ EasiLendar/www/js/controllers/AboutController.js
@@ -2,12 +2,12 @@
  * starter: Can Duy Cat
  * owner: Can Duy Cat
  * everyone can update in assigned time
- * last update: 30/03/2015
+ * last update: 31/03/2015
  * type: about controller (version)
  */
 
 angular.module('MainApp.controllers.about', [])
 
 .controller("AboutController", function($scope) {
-	$scope.ver = "1.2.40.[199]";
+	$scope.ver = "1.2.40.[200]";
 })
