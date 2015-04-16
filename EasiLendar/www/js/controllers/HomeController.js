9a10f57a4b9f19dfdc1b2b5031b725115414dc43
Cấn Duy Cát <catcd_58@vnu.edu.vn>
2015-04-16 13:44:11 +0700

    Fix some icon bug, add favicon
    - Fix mising icon because of updating library
    - Change some icon
    - Add today function for home
    - Add favicon

--- EasiLendar/www/js/controllers/HomeController.js
+++ EasiLendar/www/js/controllers/HomeController.js
@@ -1,38 +1,56 @@
 /**
  * starter: Can Duy Cat
  * owner: Can Duy Cat
- * last update: 18/04/2015
+ * last update: 29/04/2015
  * type: home controller
  */
 
 angular.module('MainApp.controllers.home', [])
 
-.controller("HomeController", function($scope, $ionicPopover) {
-    var template = "";
+.controller("HomeController", function($scope, $rootScope, $ionicPopover) {
+	// today function
+	// must call the corresponding today function
+	$scope.today = function() {
+		if ($rootScope.currentState == "month") {
+			$rootScope.monthToday();
+		}
+		else if ($rootScope.currentState == "day") {
+			$rootScope.dayToday();
+		}
+		else if ($rootScope.currentState == "list") {
+			$rootScope.listToday();
+		}
+		else if ($rootScope.currentState == "week") {
+			$rootScope.weekToday();
+		}
+		else {};
+	};
+
+	var template = "";
 
 	/**
-     * calendar select popover
-     */
-    $scope.calendarPopover = $ionicPopover.fromTemplate(template, {
-        scope: $scope,
-    });
+	 * calendar select popover
+	 */
+	$scope.calendarPopover = $ionicPopover.fromTemplate(template, {
+		scope: $scope,
+	});
 
-    // .fromTemplateUrl() method
-    $ionicPopover.fromTemplateUrl('templates/calendar-select-popover.html', {
-        scope: $scope,
-    }).then(function(popover) {
-        $scope.calendarPopover = popover;
-    });
+	// .fromTemplateUrl() method
+	$ionicPopover.fromTemplateUrl('templates/calendar-select-popover.html', {
+		scope: $scope,
+	}).then(function(popover) {
+		$scope.calendarPopover = popover;
+	});
 
-    $scope.selectPopover = function($event) {
-        $scope.calendarPopover.show($event);
-    };
-    $scope.closeSelect = function() {
-        $scope.calendarPopover.hide();
-    };
-    // Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function() {
-        $scope.calendarPopover.remove();
-    });
+	$scope.selectPopover = function($event) {
+		$scope.calendarPopover.show($event);
+	};
+	$scope.closeSelect = function() {
+		$scope.calendarPopover.hide();
+	};
+	// Cleanup the popover when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.calendarPopover.remove();
+	});
 })
 
