327fb3464d7270d38dc9a1f82f63e330bc477df9
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-04-27 12:39:35 +0700

    1.3.53.[230] MultiCalendarTest + update EasiLendarClassTest

--- Unit_Testing/jasmine/js/shareds/EasiLendarClassTest.js
+++ Unit_Testing/jasmine/js/shareds/EasiLendarClassTest.js
@@ -348,12 +348,11 @@ describe('EasiLendar Classes Test', function() {
 			date3 = new Date(2015,3,22);
 			date4 = new Date(2015,5,22);
 			
-			var arr = [];
-			arr[date1] = [event2];
-			arr[date2] = [event2];
-			arr[date3] = [event1];
-			arr[date4] = [event3];
-			eUser.uGmailCalendar = arr;
+			eUser.uGmailCalendar = [];
+			eUser.uGmailCalendar[date1] = [event2];
+			eUser.uGmailCalendar[date2] = [event2];
+			eUser.uGmailCalendar[date3] = [event1];
+			eUser.uGmailCalendar[date4] = [event3];
 		});
 		
 		it("should create Day object", function() {
@@ -521,10 +520,278 @@ describe('EasiLendar Classes Test', function() {
 				expect(pd.date).toBe(21);
 			});
 		});
+		
+		afterEach(function() {
+			eUser.uGmailCalendar = null;
+		});
 	});
 	
 	describe("Week Class", function() {
+		var date1 = new Date(2015,3,27);
+		var date2 = new Date(2015,3,25);
+		var days1 = [];
+		var days2 = [];
+		beforeEach(function() {
+			days1[0] = eEasiLendar.newDay(date1);
+			for (var i = 1; i < 7; i++) {
+				days1[i] = days1[i-1].nextDay();
+			}
+			days2[0] = eEasiLendar.newDay(date2);
+			for (var i = 1; i < 7; i++) {
+				days2[i] = days2[i-1].nextDay();
+			}
+		});
+		describe("Missing argument", function() {
+			var week;
+			beforeEach(function() {
+				week = eEasiLendar.newWeek();
+			});
+			it("should create Week object", function() {
+				expect(week).toBeDefined();
+			});
+			it("should be a current week", function() {
+				var curDate = new Date();
+				var d = (curDate.getDay() + 6) % 7; // 0(Mon) - 6(Sun)
+				var mon = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() - d);
+				var sun = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + 6 - d);
+				expect(week.days[0].date).toBe(mon.getDate());
+				expect(week.days[6].date).toBe(sun.getDate());
+			});
+			it("default start day of the week should be Monday", function() {
+				expect(week.days[0].day).toBe("Mon");
+			});
+			it("days array should contains 7 Day objects", function() {
+				expect(week.days[0].year).toBeDefined();
+				expect(week.days[4].month).toBeDefined();
+				expect(week.days[5].date).toBeDefined();
+				expect(week.days[6].prevDay().date).toEqual(week.days[5].date);
+			});
+			it("month1 should be month of Monday", function() {
+				expect(week.month1).toEqual(week.days[0].month);
+			});
+			it("month2 should be month of Sunday", function() {
+				expect(week.month2).toEqual(week.days[6].month);
+			});
+			it("year1 should be year of Monday", function() {
+				expect(week.year1).toEqual(week.days[0].year);
+			});
+			it("year2 should be year of Sunday", function() {
+				expect(week.year2).toEqual(week.days[6].year);
+			});
+			it("nextWeek function should return Week object which is the next week of current week", function() {
+				var next = week.nextWeek();
+				expect(next.days[0].date).toEqual(week.days[6].nextDay().date);
+			});
+			it("prevWeek function should return Week object which is the previous week of current week", function() {
+				var prev = week.prevWeek();
+				expect(prev.days[6].date).toEqual(week.days[0].prevDay().date);
+			});
+		});
 		
+		describe("Missing 'days' argument, 'start' argument is Sunday", function() {
+			var week;
+			beforeEach(function() {
+				week = eEasiLendar.newWeek(null, "Sun");
+			});
+			it("should create Week object", function() {
+				expect(week).toBeDefined();
+			});
+			it("should be a current week", function() {
+				var curDate = new Date();
+				var d = curDate.getDay(); // 0(Sun) - 6(Sat)
+				var sun = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() - d);
+				var sat = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + 6 - d);
+				expect(week.days[0].date).toBe(sun.getDate());
+				expect(week.days[6].date).toBe(sat.getDate());
+			});
+			it("start day of the week should be Sunday", function() {
+				expect(week.days[0].day).toBe("Sun");
+			});
+			it("days array should contains 7 Day objects", function() {
+				expect(week.days[0].year).toBeDefined();
+				expect(week.days[4].month).toBeDefined();
+				expect(week.days[5].date).toBeDefined();
+				expect(week.days[6].prevDay().date).toEqual(week.days[5].date);
+			});
+			it("month1 should be month of Sunday", function() {
+				expect(week.month1).toEqual(week.days[0].month);
+			});
+			it("month2 should be month of Sartuday", function() {
+				expect(week.month2).toEqual(week.days[6].month);
+			});
+			it("year1 should be year of Sunday", function() {
+				expect(week.year1).toEqual(week.days[0].year);
+			});
+			it("year2 should be year of Sartuday", function() {
+				expect(week.year2).toEqual(week.days[6].year);
+			});
+			it("nextWeek function should return Week object which is the next week of current week", function() {
+				var next = week.nextWeek();
+				expect(next.days[0].date).toEqual(week.days[6].nextDay().date);
+			});
+			it("prevWeek function should return Week object which is the previous week of current week", function() {
+				var prev = week.prevWeek();
+				expect(prev.days[6].date).toEqual(week.days[0].prevDay().date);
+			});
+		});
+		
+		describe("'days' argument is array of 7 days start at 2015/04/27, 'start' is missing", function() {
+			var week;
+			beforeEach(function() {
+				week = eEasiLendar.newWeek(days1);
+			});
+			it("should create Week object", function() {
+				expect(week).toBeDefined();
+			});
+			it("should be a week that has start day is 2015/04/27", function() {
+				expect(week.days[0].date).toBe(27);
+				expect(week.days[0].year).toBe(2015);
+				expect(week.days[0].month).toBe(3);
+			});
+			it("should be a week that has last day is 2015/05/03", function() {
+				expect(week.days[6].date).toBe(3);
+				expect(week.days[6].year).toBe(2015);
+				expect(week.days[6].month).toBe(4);
+			});
+			it("start day of the week should be Monday", function() {
+				expect(week.days[0].day).toBe("Mon");
+			});
+			it("days array should contains 7 Day objects", function() {
+				expect(week.days[0].year).toBeDefined();
+				expect(week.days[4].month).toBeDefined();
+				expect(week.days[5].date).toBeDefined();
+				expect(week.days[6].prevDay().date).toEqual(week.days[5].date);
+			});
+			it("month1 should be April", function() {
+				expect(week.month1).toBe(3);
+			});
+			it("month2 should be May", function() {
+				expect(week.month2).toBe(4);
+			});
+			it("year1 should be 2015", function() {
+				expect(week.year1).toBe(2015);
+			});
+			it("year2 should be 2015", function() {
+				expect(week.year2).toBe(2015);
+			});
+			describe("nextWeek function", function() {
+				var next;
+				beforeEach(function() {
+					week = eEasiLendar.newWeek(days1);
+					next = week.nextWeek();
+				});
+				it("first day should be Monday 2015/05/04", function() {
+					expect(next.days[0].date).toBe(4);
+					expect(next.days[0].day).toBe("Mon");
+					expect(next.days[0].month).toBe(4);
+					expect(next.days[0].year).toBe(2015);
+				});
+				it("last day should be Sunday 2015/05/10", function() {
+					expect(next.days[6].date).toBe(10);
+					expect(next.days[6].day).toBe("Sun");
+					expect(next.days[6].month).toBe(4);
+					expect(next.days[6].year).toBe(2015);
+				});
+			});
+			describe("prevWeek function", function() {
+				var prev;
+				beforeEach(function() {
+					week = eEasiLendar.newWeek(days1);
+					prev = week.prevWeek();
+				});
+				it("first day should be Monday 2015/04/20", function() {
+					expect(prev.days[0].date).toBe(20);
+					expect(prev.days[0].day).toBe("Mon");
+					expect(prev.days[0].month).toBe(3);
+					expect(prev.days[0].year).toBe(2015);
+				});
+				it("last day should be Sunday 2015/04/26", function() {
+					expect(prev.days[6].date).toBe(26);
+					expect(prev.days[6].day).toBe("Sun");
+					expect(prev.days[6].month).toBe(3);
+					expect(prev.days[6].year).toBe(2015);
+				});
+			});
+		});
+		
+		describe("'days' argument is array of 7 days start at 2015/04/25, 'start' is Monday", function() {
+			var week;
+			beforeEach(function() {
+				week = eEasiLendar.newWeek(days2, "Mon");
+			});
+			it("should create Week object", function() {
+				expect(week).toBeDefined();
+			});
+			it("should be a week that has start day is 2015/04/25", function() {
+				expect(week.days[0].date).toBe(25);
+				expect(week.days[0].year).toBe(2015);
+				expect(week.days[0].month).toBe(3);
+			});
+			it("should be a week that has last day is 2015/05/01", function() {
+				expect(week.days[6].date).toBe(1);
+				expect(week.days[6].year).toBe(2015);
+				expect(week.days[6].month).toBe(4);
+			});
+			it("start day of the week should be Sartuday, 'start' argument should be ignored", function() {
+				expect(week.days[0].day).toBe("Sat");
+			});
+			it("days array should contains 7 Day objects", function() {
+				expect(week.days[0].year).toBeDefined();
+				expect(week.days[4].month).toBeDefined();
+				expect(week.days[5].date).toBeDefined();
+				expect(week.days[6].prevDay().date).toEqual(week.days[5].date);
+			});
+			it("month1 should be April", function() {
+				expect(week.month1).toBe(3);
+			});
+			it("month2 should be May", function() {
+				expect(week.month2).toBe(4);
+			});
+			it("year1 should be 2015", function() {
+				expect(week.year1).toBe(2015);
+			});
+			it("year2 should be 2015", function() {
+				expect(week.year2).toBe(2015);
+			});
+			describe("nextWeek function", function() {
+				var next;
+				beforeEach(function() {
+					week = eEasiLendar.newWeek(days2);
+					next = week.nextWeek();
+				});
+				it("first day should be Sartuday 2015/05/02", function() {
+					expect(next.days[0].date).toBe(2);
+					expect(next.days[0].day).toBe("Sat");
+					expect(next.days[0].month).toBe(4);
+					expect(next.days[0].year).toBe(2015);
+				});
+				it("last day should be Friday 2015/05/08", function() {
+					expect(next.days[6].date).toBe(8);
+					expect(next.days[6].day).toBe("Fri");
+					expect(next.days[6].month).toBe(4);
+					expect(next.days[6].year).toBe(2015);
+				});
+			});
+			describe("prevWeek function", function() {
+				var prev;
+				beforeEach(function() {
+					week = eEasiLendar.newWeek(days2);
+					prev = week.prevWeek();
+				});
+				it("first day should be Monday 2015/04/18", function() {
+					expect(prev.days[0].date).toBe(18);
+					expect(prev.days[0].day).toBe("Sat");
+					expect(prev.days[0].month).toBe(3);
+					expect(prev.days[0].year).toBe(2015);
+				});
+				it("last day should be Sunday 2015/04/24", function() {
+					expect(prev.days[6].date).toBe(24);
+					expect(prev.days[6].day).toBe("Fri");
+					expect(prev.days[6].month).toBe(3);
+					expect(prev.days[6].year).toBe(2015);
+				});
+			});
+		});
 	});
 	
 	describe("WeekCalendar Class", function() {
