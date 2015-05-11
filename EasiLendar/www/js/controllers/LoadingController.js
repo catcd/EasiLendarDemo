c07df4fd0fd5424d124f78b539806e2af9774f48
Cấn Duy Cát <catcd_58@vnu.edu.vn>
2015-05-12 01:50:05 +0700

    Add loading controller test

--- EasiLendar/www/js/controllers/LoadingController.js
+++ EasiLendar/www/js/controllers/LoadingController.js
@@ -1,48 +1,40 @@
 /**
  * starter: Can Duy Cat
  * owner: Can Duy Cat
- * last update: 29/04/2015
+ * last update: 11/05/2015
  * type: loading controller
  */
 
 angular.module('MainApp.controllers.loading', [])
-
 .controller("LoadingController", function($scope, $rootScope, $cordovaNetwork, $ionicLoading, eSettings) {
-	// inject services
-	var eSettings = eSettings;
-
 	$ionicLoading.show({
 		duration: 4000,
 		template: '<ion-content class="easi-full-blue"><center><img src="img/logo.png" class="easi-logo"><br><p class="easi-font easi-big-name">EasiLendar</p><br><div id="followingBallsG"><div id="followingBallsG_1" class="followingBallsG"></div><div id="followingBallsG_2" class="followingBallsG"></div><div id="followingBallsG_3" class="followingBallsG"></div><div id="followingBallsG_4" class="followingBallsG"></div></div></center></ion-content>'
 	});
 
 	//window.onload = $scope.loadingFunction;
-
 	$scope.loadingFunction = function() {
-		if (navigator.connection != undefined) checkConnection();		// if not working, skip it
-		getTimezone();
+		if (navigator.connection != undefined) { // if not working, skip it
+			$scope.checkConnection();
+		};
+
+		$scope.getTimezone();
 		$rootScope.goToState("form");
 	}
 
-	$rootScope.$on('$stateChangeStart', function(event, toState) {
-		if (toState.name == 'loading') {
-			$rootScope.goToState("form");
+	$scope.checkConnection = function() {
+		var type = $cordovaNetwork.getNetwork();
+
+		if ((type == Connection.CELL_2G) || (type == Connection.CELL_3G) || (type == Connection.CELL_4G) || (type == Connection.CELL)) {
+			eSettings.sInternet = "3G";
+		} else if ((type == Connection.ETHERNET) || (type == Connection.WIFI)) {
+			eSettings.sInternet = "wifi";
+		} else { //((type == Connection.UNKNOWN) || (type == Connection.NONE))
+			eSettings.sInternet = "none";
 		}
-	});
-
-	function checkConnection() {
-	    var type = $cordovaNetwork.getNetwork();
-
-	    if ((type == Connection.CELL_2G) || (type == Connection.CELL_3G) || (type == Connection.CCELL_4G) || (type == Connection.CELL)) {
-	        eSettings.sInternet = "3G";
-	    } else if ((type == Connection.ETHERNET) || (type == Connection.WIFI)) {
-	        eSettings.sInternet = "wifi";
-	    } else { //((type == Connection.UNKNOWN) || (type == Connection.NONE))
-	        eSettings.sInternet = "none";
-	    }
 	}
 
-	function getTimezone() {
+	$scope.getTimezone = function() {
 		var d = new Date();
 		var n = d.getTimezoneOffset();
 		eSettings.sTimeZone = -n / 60;
